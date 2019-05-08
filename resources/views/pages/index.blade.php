143d82efd557bc81927de7eeff9de91649b432d0
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-05-08 14:49:14 +0200

    vue installed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,7 @@
     ||(view folder)so only the content has to be changed-->
     @section('headsection')
         <head>
+            <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
             <!--<h1>{{$title}}</h1>$title take place in PagesController it is good for an often used content-->
         </head>
     @endsection
