b52e6818da1f08da2b05fba3d367deab3b722d33
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-05-09 15:31:35 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/bootstrap-social.css') }}" rel="stylesheet">
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-        <link rel="shortcut icon" sizes="16x16" href="/images/img-o.jpg" />
+        <link rel="shortcut icon" sizes="16x16" href="/images/img-0.jpg" />
     </head>
     <header>
         @yield('headsection')
