26c21717fce047cc118dbc0a1a0031cddb327d24
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-05-09 13:08:49 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,8 +33,7 @@
         @yield('content')
         @yield('bodysection')
 
-                <!-- Scripts 
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.9/dist/vue.js"></script>-->
+                <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/custom.js') }}" defer></script>
 
